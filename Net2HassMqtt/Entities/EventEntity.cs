29574001be846d3b6971609585f1173dc0c7d3cb
Rob Smyth <RobSmyth@users.noreply.github.com>
2024-07-26 12:30:11 +1000

    Refactoring

--- Net2HassMqtt/Entities/EventEntity.cs
+++ Net2HassMqtt/Entities/EventEntity.cs
@@ -5,7 +5,7 @@ using NoeticTools.Net2HassMqtt.Mqtt.Payloads.Discovery;
 
 namespace NoeticTools.Net2HassMqtt.Entities;
 
-internal sealed class EventEntity : StateEntityBase<EventConfig>
+internal sealed class EventEntity : EventEntityBase<EventConfig>
 {
     public EventEntity(EventConfig config, string entityUniqueId, string deviceNodeId, INet2HassMqttClient mqttClient, ILogger logger) :
         base(config, entityUniqueId, deviceNodeId, mqttClient, logger)
