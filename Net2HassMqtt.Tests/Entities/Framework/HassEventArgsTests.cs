bbb44ede09839e6f6d98977a9839077447ae7fd3
Rob Smyth <RobSmyth@users.noreply.github.com>
2024-08-04 19:05:35 +1000

    Working on event entity

---
+++ Net2HassMqtt.Tests/Entities/Framework/HassEventArgsTests.cs
@@ -0,0 +1,23 @@
+﻿using NoeticTools.Net2HassMqtt.Entities.Framework;
+
+
+namespace Net2HassMqtt.Tests.Entities.Framework;
+
+public enum TestEventTypes1
+{
+    EventType1 = 3,
+    EventType2 = 5
+}
+
+[TestFixture]
+internal class HassEventArgsTests
+{
+    [TestCase(TestEventTypes1.EventType1, 3)]
+    [TestCase(TestEventTypes1.EventType2, 5)]
+    public void Tests(TestEventTypes1 eventType, int expectedId)
+    {
+        var target = new HassEventArgs<TestEventTypes1>(eventType);
+
+        Assert.That(expectedId, Is.EqualTo(target.EventTypeId));
+    }
+}
