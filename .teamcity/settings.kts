d7e160f26d94b0e99dfb40723c1aa1b9b8ec7a72
RobSmyth@GitHub.com <RobSmyth@GitHub.com@TeamCity>
2024-12-29 10:07:50 +1100

    TeamCity change in 'NoeticTools.Net2HassMqtt' project: runners of 'Build (uses local nuget repo)' build configuration were updated

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -34,7 +34,7 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 'Debug' option is available in the context menu for the task.
 */
 
-version = "2024.03"
+version = "2024.12"
 
 project {
 
@@ -111,7 +111,7 @@ object Build : BuildType({
             id = "Build"
             projects = "Net2HassMqtt.sln"
             configuration = "Release"
-            args = "--property:Git2SemVer_UpdateHostBuildLabel=true --source:http://10.1.10.80:5000/v3/index.json --source:https://api.nuget.org/v3/index.json"
+            args = "--property:Git2SemVer_UpdateHostBuildLabel=true --source:https://api.nuget.org/v3/index.json"
             logging = DotnetBuildStep.Verbosity.Normal
         }
         dotnetPack {
