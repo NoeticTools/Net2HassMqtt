ce7ef7a0176e76b382ba29e549cc27da5c6ec2b3
RobSmyth@GitHub.com <RobSmyth@GitHub.com@TeamCity>
2026-04-13 15:29:38 +1000

    requirements of 'Build (uses local nuget repo)' build configuration were updated (TeamCity change in 'NoeticTools / Net2HassMqtt' project)

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -32,7 +32,7 @@ To debug in IntelliJ Idea, open the 'Maven Projects' tool window (View
 'Debug' option is available in the context menu for the task.
 */
 
-version = "2025.03"
+version = "2025.11"
 
 project {
 
@@ -64,7 +64,7 @@ project {
 
     cleanup {
         baseRule {
-            artifacts(builds = 20, days = 5)
+            artifacts(builds = 20, days = 5, artifactPatterns = "+:**/*")
         }
     }
 }
@@ -122,5 +122,9 @@ object Build : BuildType({
     features {
         perfmon {
         }
+    }
+
+    requirements {
+        exists("DotNetCoreSDK10.0_Path")
     }
 })
